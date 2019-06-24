f263c415fe3d853adafaaa73b61f6b28962e7359
Bladeyy <getsuga369@gmail.com>
2019-06-24 18:51:50 +0530

    vince: Enable USB debugging at boot
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Kry9toN <dhimasbagusprayoga@gmail.com>

--- prop.mk
+++ prop.mk
@@ -11,3 +11,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
     persist.camera.dual.camera=0 \
     persist.camera.CDS=off
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
