f74b9733ae2cc0f19a1770a7a2a1b607c848cdfa
Dyneteve <dyneteve@pixelexperience.org>
2019-12-25 19:27:39 +0100

    vince: update dexpreopt flags
     Add NexusLauncherRelease to PRODUCT_DEXPREOPT_SPEED_APPS
     Remove dex flags, they're already enabled on Q
    
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- device.mk
+++ device.mk
@@ -143,6 +143,11 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Dex
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI \
+    NexusLauncherRelease
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
