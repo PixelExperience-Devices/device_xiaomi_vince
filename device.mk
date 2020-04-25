4be0ce93116e7762fd1de7e0a1ac5300ffcb8a81
TheHitMan7 <krtik.vrma@gmail.com>
2020-04-25 13:59:50 +0530

    vince: Add APN config
    * import custom APN config from Project Awaken
    
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>
    Signed-off-by: starlight5234 <ifist2834@gmail.com>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
    Signed-off-by: IAmSalvaMartini <starraos@gmail.com>
    Signed-off-by: rk134 <rahul.kantrapally@gmail.com>
    
    vince: import custom apns-conf.xml
    
    Signed-off-by: rk134 <rahul.kantrapally@gmail.com>
    Signed-off-by: Rahul K <rahul-k@bigdi.cc>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- device.mk
+++ device.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/apns-conf.xml:system/etc/apns-conf.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
