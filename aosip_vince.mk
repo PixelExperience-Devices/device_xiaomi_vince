410c97d2f785899d000d0ce0b54c97bd93e721d5
KenHV <kenhv@null.net>
2020-05-02 08:19:48 +0000

    vince: switch to tissot fingerprint
    just work goddammit
    
    Signed-off-by: KenHV <kenhv@null.net>

--- aosip_vince.mk
+++ aosip_vince.mk
@@ -36,9 +36,9 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V10.0.4.0.OEGMIFH release-keys"
+    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys" 
+BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
 
 TARGET_VENDOR := xiaomi
