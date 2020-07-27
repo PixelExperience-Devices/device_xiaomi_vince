a381e9a868f242b427e5888254bfa0f10709bbee
JacobDeepu <JacobDeepu@users.noreply.github.com>
2020-07-27 20:35:50 +0530

    vince: build missing libwifi-hal-ctrl
    Change-Id: I84b7a568183012680cedf86da2a1b145df6c963d
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- device.mk
+++ device.mk
@@ -202,6 +202,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+PRODUCT_PACKAGES += \
+    libwifi-hal-ctrl
+
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
