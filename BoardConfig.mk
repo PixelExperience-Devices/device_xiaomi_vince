71540119b212b539912da7f628e5a163749a8d93
starlight5234 <ifist2834@gmail.com>
2020-08-22 15:18:30 +0530

    BoardConfig: Inline with StormBreaker Kranul
    StormBreaker is lob
    
    Signed-off-by: starlight5234 <ifist2834@gmail.com>
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
 # Kernel
-TARGET_KERNEL_CONFIG := vince_defconfig
+TARGET_KERNEL_CONFIG := vince-perf_defconfig
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.usbconfigfs=true loop.max_part=16
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
