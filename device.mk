f3c3b76f36f4c6a308dc0f7912b2784dc3255555
Luchetto <lucchetto.tie@live.com>
2020-09-14 00:42:42 +0800

    vince: Build system and vendor copy of libhwbinder
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: starlight5234 <ifist2834@gmail.com>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- device.mk
+++ device.mk
@@ -126,6 +126,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder \    
+    libhwbinder.vendor
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     libldacBT_dec
