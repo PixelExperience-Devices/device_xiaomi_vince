600b85401bb0e18ffa92a394ae97d36304a6d581
Luchetto <lucchetto.tie@live.com>
2020-09-14 00:51:31 +0800

    [DNM] vince: copy prebuilt libprotobuf from vndk
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: starlight5234 <ifist2834@gmail.com>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- device.mk
+++ device.mk
@@ -403,6 +403,13 @@ PRODUCT_PACKAGES += \
     vndk_package \
     libstdc++.vendor
 
+# VNDK protobuff
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+
 # VR
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
