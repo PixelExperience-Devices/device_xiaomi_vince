2fca8b5600e1684c93d335862f71397b6af5e7cf
Luchetto <lucchetto.tie@live.com>
2020-09-19 19:38:22 +0530

    vince: Copy Prebuilt camera provider
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- device.mk
+++ device.mk
@@ -142,8 +142,6 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     camera.msm8953 \
     libmm-qcamera \
     Snap
