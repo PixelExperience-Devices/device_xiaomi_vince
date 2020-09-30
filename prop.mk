BlissRoms-Devices/android_device_xiaomi_vince

vince: disable cds module
* fixes green artifacts in camera

Signed-off-by: KenHV <kenhv@null.net>

prop.mk
@@ -54,7 +54,8 @@ persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2 \
 persist.camera.HAL3.enabled=1 \
-persist.camera.eis.enable=1
+persist.camera.eis.enable=1 \
+persist.camera.CDS=off
 
 # Cne
 PRODUCT_PROPERTY_OVERRIDES += \

PixelExperience-Devices/device_xiaomi_vince

Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

prop.mk
@@ -53,7 +53,8 @@ persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2 \
-persist.camera.HAL3.enabled=1
+persist.camera.HAL3.enabled=1 \
+persist.camera.CDS=off
 
 # Cne
 PRODUCT_PROPERTY_OVERRIDES += \