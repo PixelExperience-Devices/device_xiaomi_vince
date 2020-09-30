10f168b5825a1a60fcae65e5e476ea6a361be15e
Alexandru Scurtu <sasha.scurtu@gmail.com>
2020-09-30 17:00:37 +0000

    vince: prop: revert dalvik config changes

--- prop.mk
+++ prop.mk
@@ -9,12 +9,12 @@ dalvik.vm.image-dex2oat-filter=speed
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.heapstartsize=8m \
+dalvik.vm.heapstartsize=16m \
 dalvik.vm.heapgrowthlimit=256m \
 dalvik.vm.heapsize=512m \
-dalvik.vm.heaptargetutilization=0.9 \
-dalvik.vm.heapminfree=8m \
-dalvik.vm.heapmaxfree=16m
+dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapminfree=4m \
+dalvik.vm.heapmaxfree=8m
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
