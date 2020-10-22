930353373c2ecfc3bce90b22fbdc597ad89f4f2b
starlight5234 <starlight5234@protonmail.ch>
2020-10-22 17:17:44 +0200

    vince: Switch to October Coral fp
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- derp_vince.mk
+++ derp_vince.mk
@@ -44,4 +44,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V11.0.2.0.OEGMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys"
