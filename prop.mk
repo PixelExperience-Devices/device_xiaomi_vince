6032d0c8e7040d48430382185e3968738cedf5fc
ZeeLog <Danjel.Zammit@gmail.com>
2020-12-05 11:09:06 +0100

    vince: ims: silence all ims dpl related logspam
    Change-Id: Ibefeb846239cd9e33d95728dcf67f5f9553a2b2a
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- prop.mk
+++ prop.mk
@@ -229,6 +229,15 @@ ro.telephony.use_old_mnc_mcc_format=true \
 service.qti.ims.enabled=1 \
 vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
 
+# Radio (IMS)
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.ims.disableIMSLogs=1 \
+persist.vendor.ims.disableADBLogs=1 \
+persist.vendor.ims.disableDebugDataPathLogs=1 \
+persist.vendor.ims.disableDebugLogs=1 \
+persist.vendor.ims.disableSigHandler=1 \
+persist.vendor.ims.disableQXDMLogs=1
+
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.sf.early_phase_offset_ns=1500000 \
