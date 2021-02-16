791cff80861575c7bcffb9ca4efa7c2c8aef555e
Justin Yun <justinyun@google.com>
2021-02-16 10:57:13 +0000

    vince: Inherit base system_ext.mk files for aosp targets
    Make aosp targets inherit handheld_system_ext.mk and
    telephony_system_ext.mk files.
    Devices that have /system_ext or /system/system_ext must inherit any
    of *_system_ext.mk files to install mandatory packages for system_ext
    partition.
    
    Bug: 144542478
    Test: Build aosp targets
    Change-Id: Ie160998150caa953c9a34726525cdbcefc222841
    Merged-In: Ie160998150caa953c9a34726525cdbcefc222841
    (cherry picked from commit 3464451392891c13d458c6a48a7f66b7e234959a)

--- derp_vince.mk
+++ derp_vince.mk
@@ -18,6 +18,12 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
