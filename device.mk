15f37adaed621394a38b15ae76528f757c9a1352
starlight5234 <starlight5234@protonmail.ch>
2021-03-08 16:54:37 +0530

    Revert "[DNM] vince: Build widevine 1.3"
    - Actual reason of crash was enforcing.
    
    This reverts commit bfc907e14e5357e396e53b070e634b81bb8060fd.
    
    Change-Id: Ifd1220bf005d856ee8317b5e5f05dc63355d7562
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- device.mk
+++ device.mk
@@ -190,7 +190,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.3-service.widevine \
     android.hardware.drm@1.3-service-lazy.clearkey
 
 # Fingerprint
