28f71952d4639ff584e6969bb9409b917e44a14e
adnan-44 <mohd.adnan0405@gmail.com>
2021-03-10 01:54:50 +0530

    vince: drop TARGET_USES_BLUR flag
    Derpfest vendor no longer support this flag https://github.com/DerpFest-11/vendor_derp/commit/3014181e1dedee4d38dbebaa79a8a50639414cb7.

--- derp_vince.mk
+++ derp_vince.mk
@@ -24,7 +24,6 @@ $(call inherit-product, device/xiaomi/vince/device.mk)
 # Inherit some common RevengeOS stuff.
 $(call inherit-product, vendor/derp/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1440
-TARGET_USES_BLUR := false
 TARGET_DISABLE_POSTRENDER_CLEANUP := true
 
 # Derpfest Official
