d5cc8baec0ad04cc8cd0d33d34d4ed630b055355
Albert I <kras@raphielgang.org>
2021-06-26 15:30:31 +0800

    vince: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: orges <me@orgesified.ml>
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- device.mk
+++ device.mk
@@ -93,6 +93,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/etc/apns-conf.xml:system/etc/apns-conf.xml
 
+# ART
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
