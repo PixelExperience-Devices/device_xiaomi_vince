150b60dcef29fc54f5f3176b7ac50e2619876754
viralbanda <viralbandachannel@gmail.com>
2021-06-30 07:18:51 +0000

    vince: improve scrolling
    - added additional props to improve scrolling
    - better ram
    
    Signed-off-by: PrajjuS <theprajjus@gmail.com>
    Signed-off-by: Rahul K <rahul-k@bigdi.cc>

--- prop.mk
+++ prop.mk
@@ -308,3 +308,22 @@ persist.vendor.usb.config.extra=none
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
+
+# Additional Props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.cgroup_follow.enable=true
+    persist.vendor.qti.inputopts.enable=true
+    persist.vendor.qti.inputopts.movetouchslop=0.6
+    ro.qcom.adreno.qgl.ShaderStorageImageExtendedFormats=0
+
+# Better Ram
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.fha_enable=true \
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
