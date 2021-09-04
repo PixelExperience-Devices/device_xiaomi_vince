cded888ef0bc86a58420374b171146eb9c663710
dib12 <61652361+dib12@users.noreply.github.com>
2021-09-04 09:34:39 +0530

    msm8953-common: Drop 3.18 kernel support
    * remove init.qcom.bt.sh from product package.can't buildable
    
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- msm8953.mk
+++ msm8953.mk
@@ -126,7 +126,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
-	gralloc.msm8953
+    gralloc.msm8953
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
@@ -273,7 +273,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.qcom.sh \
-    init.qcom.bt.sh \
     init.qcom.post_boot.sh
 
 # RenderScript
