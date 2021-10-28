3022efa9b01f649650743f1f593f79b1f3202fb8
gamehackerever <gamehackerever@gmail.com>
2021-10-28 21:04:20 +0530

    vince: prop: Use phase offsets as duration
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Adopt the new way of specifying
    phase offsets to SurfaceFlinger.
    
    Bug: 158464992
    Test: scrolling, app launching/closing, playing video
    Change-Id: Id7b5c765e24b763a0bad75af63931819d2d8d765
    Signed-off-by: PrajjuS <theprajjus@gmail.com>
    Signed-off-by: Rahul K <rahul-k@bigdi.cc>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- prop.mk
+++ prop.mk
@@ -280,10 +280,13 @@ persist.vendor.ims.disableQXDMLogs=1
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.early_phase_offset_ns=1500000 \
-debug.sf.early_app_phase_offset_ns=1500000 \
-debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000 \
+debug.sf.use_phase_offsets_as_durations=1 \
+debug.sf.late.sf.duration=10500000 \
+debug.sf.late.app.duration=20500000 \
+debug.sf.early.sf.duration=21000000 \
+debug.sf.early.app.duration=16500000 \
+debug.sf.earlyGl.sf.duration=13500000 \
+debug.sf.earlyGl.app.duration=21000000 \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.max_virtual_display_dimension=4096 \
 ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
