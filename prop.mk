f19953299d4564312c87fee87a9f3969ace0bd66
Sonal Singh <sonal.singh.19993@gmail.com>
2021-10-28 21:22:33 +0530

    vince: prop: Enable round corners in Gboard
    Change-Id: I06194f5204a27b8930c658089fba080ac94da071
    Signed-off-by: PrajjuS <theprajjus@gmail.com>
    Signed-off-by: Rahul K <rahul-k@bigdi.cc>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- prop.mk
+++ prop.mk
@@ -190,6 +190,11 @@ ro.fm.transmitter=false
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.frp.pst=/dev/block/bootdevice/by-name/config
 
+# Gboard
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.com.google.ime.kb_pad_port_b=1 \
+ro.com.google.ime.corner_key_r=35
+
 # Logging
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.control_privapp_permissions=log
