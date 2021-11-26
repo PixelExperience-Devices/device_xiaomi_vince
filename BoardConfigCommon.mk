b0437d4061b4dcc286bdb4b87f2b2b5fb1362fe3
dib12 <61652361+dib12@users.noreply.github.com>
2021-11-26 19:23:30 +0530

    msm8953-common: Add DT2W support for vince
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,6 +108,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/platform/soc/78b7000.i2c/i2c-3/3-0020/input/input2/wake_gesture"
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
