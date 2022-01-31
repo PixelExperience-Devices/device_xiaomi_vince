8e1f833f88719343f6e136cdc9e167b982b6f1b4
Chenyang Zhong <zhongcy95@gmail.com>
2022-01-31 20:48:27 +0530

    vince: Set BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
    Signed-off-by: andro-sapien <alanmadathil007@gmail.com>
    Signed-off-by: PrajjuS <theprajjus@gmail.com>
    Change-Id: I0899e14c62f4e84d8dadcd69c13ae38d981d70b5
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -209,6 +209,7 @@ VENDOR_SECURITY_PATCH := 2021-07-05
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 #SELINUX_IGNORE_NEVERALLOWS := true
+BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
 
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
