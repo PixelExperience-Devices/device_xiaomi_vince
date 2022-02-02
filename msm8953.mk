59e669cfa1ec02b3b1780de49a388ca4c8c2d5ec
ZVNexus <akoskovich@pm.me>
2022-02-02 14:36:49 +0530

    msm8953-common: Remove vendor RenderScript implementation
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Change-Id: Ia2cb9394fdc65148100032620aabc306f7b3c1e7
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- msm8953.mk
+++ msm8953.mk
@@ -270,10 +270,6 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.post_boot.sh
 
-# RenderScript
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
