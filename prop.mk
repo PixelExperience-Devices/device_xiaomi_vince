2c9e0c17809e9e3b6cfd05c7610052c5cfb6e2b6
clarencelol <clarencekuiek@icloud.com>
2022-02-13 20:06:23 +0800

    vince: display: properties: Disable SDM Scalar
    * Else, ScalarConfig: :GetPipeScaleSettingsInfo: lib_scale_get_pipe_settings failed: status = 2
    
    Change-Id: I3c79a7cc561e6ba4c940bd418ae04921a795081c
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- prop.mk
+++ prop.mk
@@ -152,6 +152,7 @@ debug.sf.enable_hwc_vds=1 \
 debug.sf.gpu_comp_tiling=1 \
 debug.sf.hw=1 \
 debug.sf.latch_unsignaled=1 \
+vendor.display.disable_scaler=1 \
 debug.sf.recomputecrop=0 \
 dev.pm.dyn_samplingrate=1 \
 persist.demo.hdmirotationlock=false \
