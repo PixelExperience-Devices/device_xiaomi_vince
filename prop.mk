f7a4b5050304870a30f28f5cf3bcf30791523f4a
SamarV-121 <samarvispute121@pm.me>
2022-02-13 23:16:01 +0800

    vince: display: properties: Remove obsolete
     debug.sf.disable_backpressure
    
     * It has been removed in Android S
     * https://android.googlesource.com/platform/frameworks/native/+/2ec5391acfc340c2e195d573cf5a078ff0f280d2
    
    Change-Id: I23a210a37ad257f0c8b7a3c5e1d59b2ac20cde9a
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- prop.mk
+++ prop.mk
@@ -147,7 +147,6 @@ debug.hwui.use_buffer_age=false \
 debug.mdpcomp.idletime=600 \
 debug.mdpcomp.logs=0 \
 debug.performance.tuning=1 \
-debug.sf.disable_backpressure=1 \
 debug.sf.disable_hwc=0 \
 debug.sf.enable_hwc_vds=1 \
 debug.sf.gpu_comp_tiling=1 \
