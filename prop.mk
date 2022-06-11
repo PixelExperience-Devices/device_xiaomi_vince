7793b5c423b24e20c8fc7b2106e4125acf722982
rk134 <rahul-k@bigdi.cc>
2022-06-11 20:47:45 +0100

    vince: vendor.prop: rework SF properties
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- prop.mk
+++ prop.mk
@@ -281,19 +281,24 @@ persist.vendor.ims.disableSigHandler=1 \
 persist.vendor.ims.disableQXDMLogs=1
 
 # SurfaceFlinger
-PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.early_phase_offset_ns=1500000 \
-debug.sf.early_app_phase_offset_ns=1500000 \
-debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000 \
+PRODUCT_VENDOR_PROPERTIES += \
+debug.sf.use_phase_offsets_as_durations=1 \
+debug.sf.late.sf.duration=10500000 \
+debug.sf.late.app.duration=20500000 \
+debug.sf.early.sf.duration=21000000 \
+debug.sf.early.app.duration=16500000 \
+debug.sf.earlyGl.sf.duration=13500000 \
+debug.sf.earlyGl.app.duration=21000000 \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
 ro.surface_flinger.max_virtual_display_dimension=4096 \
 ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 ro.surface_flinger.protected_contents=true \
 ro.surface_flinger.use_content_detection_for_refresh_rate=true \
 ro.surface_flinger.set_display_power_timer_ms=10000 \
-ro.surface_flinger.set_touch_timer_ms=5000 \
-ro.surface_flinger.set_idle_timer_ms=9000 \
+ro.surface_flinger.set_touch_timer_ms=200 \
+ro.surface_flinger.set_idle_timer_ms=80 \
+ro.surface_flinger.enable_frame_rate_override=true \
+ro.surface_flinger.start_graphics_allocator_service=true \
 ro.surface_flinger.wcg_composition_dataspace=143261696
 
 # SOC
