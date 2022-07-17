419d15449ab32953e7f5b250b9b2679b938c6060
rk134 <rahul-k@bigdi.cc>
2022-07-17 18:45:15 +0100

    vince: Set TARGET_KERNEL_CONFIG
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,7 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_SOURCE := kernel/xiaomi/vince
+TARGET_KERNEL_CONFIG := vince-perf_defconfig
 TARGET_KERNEL_ADDITIONAL_FLAGS := AR=llvm-ar AS=llvm-as NM=llvm-nm LD=ld.lld OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump OBJSIZE=llvm-size READELF=llvm-readelf STRIP=llvm-strip HOSTAR=llvm-ar HOSTAS=llvm-as HOSTNM=llvm-nm HOSTLD=ld.lld HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Clang
