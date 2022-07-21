48d932b9e365a1d7290c5cbb95acf4d07d906e21
rk134 <rahul-k@bigdi.cc>
2022-07-21 10:31:47 +0100

    vince: drop readonly variables
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,11 +63,6 @@ TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-15
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
-# ART
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-
 # APEX
 OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 
